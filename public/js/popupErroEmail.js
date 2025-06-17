dd8e21af8bb564f085a87d1a793dd01d1240dfbd
SamuelPatricin <166259994+SamuelPatricin@users.noreply.github.com>
2025-06-17 16:38:10 -0300

    Delete public directory

--- public/js/popupErroEmail.js
+++
@@ -1,8 +0,0 @@
-export function mostrarPopupErroEmail() {
-    const popup = document.getElementById('popupErroEmail');
-    popup.classList.add('show');
-
-    setTimeout(() => {
-        popup.classList.remove('show');
-    }, 4000); // Fecha após 4 segundos
-}
